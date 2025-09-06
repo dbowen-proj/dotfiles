014aa606ea907e2afd3e3b0bdabd633f55de2b0f
Darren Bowen <dbowen@horizonview.net>
2025-09-06 11:44:14 -0400

    Updated myoptions, added hypr and i3endeavour

--- nvim/.config/nvim/after/plugin/myoptions.lua
+++ nvim/.config/nvim/after/plugin/myoptions.lua
@@ -7,3 +7,8 @@ vim.opt.expandtab = true
 
 vim.opt.number = true
 --vim.opt.relativenumber = true
+
+vim.api.nvim_set_hl(0, "Normal", { bg = "none"})
+vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none"})
+vim.api.nvim_set_hl(0, "NeoTreeNormal", { bg = "none"})
+vim.api.nvim_set_hl(0, "NeoTreeNormalNC", { bg = "none"})
