9e03a03e02b45b784488a63db08b6c0d3fcdf561
Darren Bowen <dbowen@horizonview.net>
2025-09-06 11:53:25 -0400

    Fixed no background for all splits

--- nvim/.config/nvim/after/plugin/myoptions.lua
+++ nvim/.config/nvim/after/plugin/myoptions.lua
@@ -9,6 +9,7 @@ vim.opt.number = true
 --vim.opt.relativenumber = true
 
 vim.api.nvim_set_hl(0, "Normal", { bg = "none"})
+vim.api.nvim_set_hl(0, "NormalNC", { bg = "none"})
 vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none"})
 vim.api.nvim_set_hl(0, "NeoTreeNormal", { bg = "none"})
 vim.api.nvim_set_hl(0, "NeoTreeNormalNC", { bg = "none"})
